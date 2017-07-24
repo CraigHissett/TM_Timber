4efa06645ebc5a66a4390674bc4edd597fa1caae
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-07-24 16:18:12 +0100

    Update BinSensor.py

--- BinSensor/BinSensor.py
+++ BinSensor/BinSensor.py
@@ -2,23 +2,20 @@ import RPi.GPIO as GPIO
 import time
 import Email
 
-GPIO.setmode(GPIO.BCM)
-BinNo = 1
+GPIO.setmode(GPIO.BOARD)
 
-#BCM Pin 23 == Pin 16 on header. Switch to be connected to pin 16 and 3.3v pin
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#Switch for Bin 1 to be connected to pin 16 and 3.3v pin
+#Switch for Bin 2 to be connected to pin 18 and 3.3v pin
+GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 #This function will run when the button is triggered
-def EmailFunction():
+def EmailFunction(BinNo):
   print(“Button Triggered - Bin is full!”)
   SendEmail("craighissett@gmail.com", 'Bin ' + str(BinNo) + ' is full', "Please proceed")
-  if BinNo == 1:
-    BinNo = 2
-  else:
-    BinNo = 1
-  print("Trigger 30min delay")
-  time.sleep(30)
+  print("Trigger 10min delay")
+  time.sleep(10)
   
-GPIO.add_event_detect(23, GPIO.RISING, callback=EmailFunction, bouncetime=300)
-
+GPIO.add_event_detect(16, GPIO.RISING, callback=EmailFunction(1), bouncetime=300)
+GPIO.add_event_detect(18, GPIO.RISING, callback=EmailFunction(2), bouncetime=300)
 GPIO.cleanup()

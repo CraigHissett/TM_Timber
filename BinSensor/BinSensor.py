3b7c2d375251003784d0640ef52a88dcf5286e35
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-07-25 12:48:02 +0100

    Update BinSensor.py

--- BinSensor/BinSensor.py
+++ BinSensor/BinSensor.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
+
 import RPi.GPIO as GPIO
 import time
-import Email
+from Email import*
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -10,12 +12,19 @@ GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 #This function will run when the button is triggered
-def EmailFunction(BinNo):
-  print(“Button Triggered - Bin is full!”)
-  SendEmail("craighissett@gmail.com", 'BIN ' + str(BinNo) + ' FULL - PLEASE COLLECT', "")
-  print("Trigger 10min delay")
-  time.sleep(10)
-  
-GPIO.add_event_detect(16, GPIO.RISING, callback=EmailFunction(1), bouncetime=300)
-GPIO.add_event_detect(18, GPIO.RISING, callback=EmailFunction(2), bouncetime=300)
+def Email1():
+        print ('Button Triggered - Bin 1 full!')
+        SendEmail("craighissett@gmail.com", 'BIN 1 FULL - PLEASE COLLECT', "")
+        print ('Trigger 10min delay')
+        time.sleep(10)
+
+def Email2():
+        print ('Button Triggered - Bin 2 full!')
+        SendEmail("craighissett@gmail.com", 'BIN 2 FULL - PLEASE COLLECT', "")
+        print ('Trigger 10min delay')
+        time.sleep(10)
+
+GPIO.add_event_detect(16, GPIO.RISING, callback=Email1, bouncetime=300)
+GPIO.add_event_detect(18, GPIO.RISING, callback=Email2, bouncetime=300)
+
 GPIO.cleanup()

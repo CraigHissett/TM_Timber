9195ab683046d78b98da295ddcbbf9b553b03cac
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-07-26 22:44:42 +0100

    Update BinSensor.py

--- BinSensor/BinSensor.py
+++ BinSensor/BinSensor.py
@@ -2,6 +2,7 @@
 
 import RPi.GPIO as GPIO
 import time
+from lcd import *
 from Email import SendMail
 
 GPIO.setmode(GPIO.BOARD)
@@ -11,17 +12,23 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
+lcd_string("Bin Sensor Alerts",LCD_LINE_1)
+
 #This function will run when the button is triggered
 def Email1(self):
-        print ('Button Triggered - Bin 1 full!')
+        #print ('Button Triggered - Bin 1 full!')
+        lcd_string('Bin #1 FULL',LCD_LINE_2)
         SendEmail("craighissett@gmail.com", 'BIN 1 FULL - PLEASE COLLECT', "")
-        print ('Trigger 10min delay')
+        #print ('Trigger 10min delay')
         time.sleep(10)
-
+        lcd_string('Bin #2 Filling',LCD_LINE_2)
+        time.sleep(10)
+        
 def Email2(self):
-        print ('Button Triggered - Bin 2 full!')
+        lcd_string('Bin #2 FULL',LCD_LINE_2)
         SendEmail("craighissett@gmail.com", 'BIN 2 FULL - PLEASE COLLECT', "")
-        print ('Trigger 10min delay')
+        time.sleep(10)
+        lcd_string('Bin #1 Filling',LCD_LINE_2)
         time.sleep(10)
 
 GPIO.add_event_detect(16, GPIO.RISING, callback=Email1, bouncetime=300)
@@ -29,6 +36,8 @@ GPIO.add_event_detect(18, GPIO.RISING, callback=Email2, bouncetime=300)
 
 while True:
         #print('Looping')
-        time.sleep(1)
+        lcd_string("LAN: " + get_ip_address('eth0'),LCD_LINE_3)
+        lcd_string("WLAN: " + get_ip_address('wlan0'),LCD_LINE_4) 
+        time.sleep(5)
 
 GPIO.cleanup()

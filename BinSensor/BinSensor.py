21334ea66ad1baaab176717c7c60d316f61e877c
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-08-08 17:21:46 +0100

    Update BinSensor.py

--- BinSensor/BinSensor.py
+++ BinSensor/BinSensor.py
@@ -19,17 +19,17 @@ lcd_string("    Dust-O-Matic    ",LCD_LINE_1)
 def Email1(self):
         #print ('Button Triggered - Bin 1 full!')
         lcd_string('    Bin #1 FULL     ',LCD_LINE_2)
-        SendEmail("craighissett@gmail.com", 'BIN 1 FULL - PLEASE COLLECT', "")
+        SendEmail("craighissett@gmail.com", 'TRAILER 1 FULL - PLEASE COLLECT', "")
         #print ('Trigger 10min delay')
         time.sleep(10)
-        lcd_string('   Bin #2 Filling   ',LCD_LINE_2)
+        lcd_string(' TRAILER #2 Filling ',LCD_LINE_2)
         time.sleep(10)
         
 def Email2(self):
         lcd_string('    Bin #2 FULL     ',LCD_LINE_2)
-        SendEmail("craighissett@gmail.com", 'BIN 2 FULL - PLEASE COLLECT', "")
+        SendEmail("craighissett@gmail.com", 'TRAILER 2 FULL - PLEASE COLLECT', "")
         time.sleep(10)
-        lcd_string('   Bin #1 Filling   ',LCD_LINE_2)
+        lcd_string(' TRAILER #1 Filling ',LCD_LINE_2)
         time.sleep(10)
 
 GPIO.add_event_detect(16, GPIO.RISING, callback=Email1, bouncetime=300)
@@ -37,8 +37,8 @@ GPIO.add_event_detect(18, GPIO.RISING, callback=Email2, bouncetime=300)
 
 while True:
         #print('Looping')
-        lcd_string("LAN: " + get_ip_address('eth0'),LCD_LINE_3)
-        lcd_string("WLAN: " + get_ip_address('wlan0'),LCD_LINE_4) 
+        lcd_string("LAN: " + get_ip_address('eth0'),LCD_LINE_4)
+        #lcd_string("WLAN: " + get_ip_address('wlan0'),LCD_LINE_4) 
         time.sleep(5)
 
 GPIO.cleanup()

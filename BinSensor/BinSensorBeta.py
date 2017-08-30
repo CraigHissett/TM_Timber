c1b354b56195152bdf600f79a2fbefaa971f10c0
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-08-30 19:46:06 +0100

    Update and rename BinSensor.py to BinSensorBeta.py

--- BinSensor/BinSensorBeta.py
+++ BinSensor/BinSensorBeta.py
@@ -51,8 +51,10 @@ while True:
         #lcd_string("WLAN: " + get_ip_address('wlan0'),LCD_LINE_4)
         if GPIO.event_detected(18):
                 lcd_string('TRAILER #1 TRIGGERED',LCD_LINE_2)
+                Notifier()
         if GPIO.event_detected(16):
                 lcd_string('TRAILER #2 TRIGGERED',LCD_LINE_2)
+                Notifier()
         time.sleep(0.5)
 
 GPIO.cleanup()

bd853922e5825d61114a8b30f4f67aa26e4b5200
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-08-30 23:25:46 +0100

    Update BinSensor.py

--- BinSensor/BinSensor.py
+++ BinSensor/BinSensor.py
@@ -34,10 +34,10 @@ while True:
         lcd_string("LAN: " + get_ip_address('eth0'),LCD_LINE_4)
         #lcd_string("WLAN: " + get_ip_address('wlan0'),LCD_LINE_4)
         if GPIO.event_detected(18):
-                lcd_string('TRAILER #1 TRIGGERED',LCD_LINE_2)
+                #lcd_string('TRAILER #1 TRIGGERED',LCD_LINE_2)
                 Notifier(18)
         if GPIO.event_detected(16):
-                lcd_string('TRAILER #2 TRIGGERED',LCD_LINE_2)
+                #lcd_string('TRAILER #2 TRIGGERED',LCD_LINE_2)
                 Notifier(16)
         time.sleep(0.5)
 GPIO.cleanup()

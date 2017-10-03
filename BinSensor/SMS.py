dbf39df3ec4a67dd3aabd546595f1c39081694ae
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-10-04 00:45:50 +0100

    updated

--- BinSensor/SMS.py
+++ BinSensor/SMS.py
@@ -10,12 +10,29 @@ import smtplib
 # verizon:  number@vtext.com
 # sprint:   number@page.nextel.com
 # three:    number@three.co.uk
+#           number@smtp-mbb.three.co.uk
+# vodafone: number@vodafone.net
+# ee:       number@mms.ee.co.uk
+
 # Establish a secure session with gmail's outgoing SMTP server using your gmail account
 server = smtplib.SMTP( "smtp.gmail.com", 587 )
 
 server.starttls()
 
-server.login( '<gmail_address>', '<gmail_password>' )
+server.login("user", 'pw' )
+
+efrom = "Craigistan"
+eto= ""
+esubject= "DTT"
+body = "Dusto Test Text"
+
+message = ("From: %s\r\n" % efrom
+         + "To: %s\r\n" % eto
+         + "Subject: %s\r\n" % esubject
+         + "\r\n"
+         + body)
+
 
 # Send text message through SMS gateway of destination number
-server.sendmail( '<from>', '<number>@tmomail.net', '<msg>' )
+server.sendmail(efrom, eto, message)
+print('Sent Craig2')
